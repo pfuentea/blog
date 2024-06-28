b852bd36d54616133e918f3d04ec5cc18eb13034
Patricio Fuentealba <patricio.fuentealba.feliu@gmail.com>
2024-06-27 23:25:51 -0400

    clase 27 de junio

--- miweb/blog/urls.py
+++ miweb/blog/urls.py
@@ -6,6 +6,21 @@ urlpatterns = [
     path('', views.index , name='index'),
     path('dashboard/', views.dashboard , name='dashboard'),
     path('login/', views.login , name='login'),
+    # crear un menu en bootstrap con las secciones : 
+    # Landing (index), Quienes somos(about), Contacto, Galeria
+    #crear los templates (HTML)
+    #crear las rutas (URLS.PY)
+    #crear las vistas (VIEWS.PY)
+    path('about/', views.about , name='about'),
+    path('contacto/', views.contacto , name='contacto'),
+    path('galeria/', views.galeria , name='galeria'),
+
+    path('time_display/', views.time_display , name='time_display'),
+
+    
+
+
+
     
 ]
 
--- miweb/blog/views.py
+++ miweb/blog/views.py
@@ -1,20 +1,56 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from time import gmtime,strftime,localtime
 
 def index(request):
     respuesta="<h1>Hola Curso!</h1>"
     return HttpResponse(respuesta)
 
 def dashboard(request):
-
-    return render(request, 'dashboard.html')
+    context={
+            'title':'Dashboard'
+        }
+    return render(request, 'dashboard.html', context)
 
 def login (request):
-
     context={
         'titulo':'Register',
         'nombre':'Pedro',
     }
-    
     return render(request, 'login.html',context)
+
+def about(request):
+    context={
+        'title':'About'
+    }
+    return render(request, 'about.html',context)
+
+def contacto(request):
+
+    context={
+        'title':'Contacto'
+    }
+    return render(request, 'contacto.html',context)
+
+def galeria(request):
+    context={
+        'title':'Galeria'
+    }
+    return render(request, 'galeria.html',context)
+
+# CREAR UNA RUTA time_display en la que llame a un método (VIEW) que nos diga la hora y la despliegue en el navegador, para eso usaremos la libreria time con sus métodos strftime() y gmtime()
+# hora_actual = strftime("%Y-%m-%d %H:%M %p", gmtime())
+
+def time_display(request):
+    if request.method == 'POST':
+        hora_actual ="HORA DE IRSE A CASA!"
+    elif request.method == 'GET':
+        hora_actual = strftime("%Y-%m-%d %H:%M:%S %p", localtime())
+
+    context={
+        'hora':hora_actual,
+        'title':'Time Display'
+    }
+    return render(request, 'time_display.html',context)
+    
